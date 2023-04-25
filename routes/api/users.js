5182b6bb64e4d71c9913f915f463947a2a15f683
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-04-25 10:50:24 -0400

    update import error

--- routes/api/users.js
+++ routes/api/users.js
@@ -23,6 +23,11 @@ router.get("/test", (req, res) => res.json({ msg: "Users works" }));
 router.post("/register", (req, res) => {
   const { errors, isValid } = validateRegisterInput(req.body);
 
+  // Check validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
   User.findOne({ email: req.body.email }).then((user) => {
     if (user) {
       return res.status(400).json({ email: "Email already exists" });
