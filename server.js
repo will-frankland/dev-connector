4d9aaf228737d6adfe0dda6d7b12902dc3477576
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-04-26 10:39:36 -0400

    finalize routes to delete education and experience for users

--- server.js
+++ server.js
@@ -33,6 +33,6 @@ app.use('/api/users', users);
 app.use('/api/profile', profile);
 app.use('/api/posts', posts);
 
-const port = process.env.PORT || 5002;
+const port = process.env.PORT || 5003;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
