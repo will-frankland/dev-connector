5c32712deefb54143231f660a7b9bde480d5a2d1
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-04-26 11:10:50 -0400

    create route for newPost

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,9 +1,28 @@
 const express = require('express');
 const router = express.Router();
+const mogoose = require('mongoose');
+const passport = require('passport');
+
+const Post = require('../../models/Post');
 
 // @route     GET api/posts/test
 // @desc      Tests post route
 // @access    Public
 router.get('/test', (req, res) => res.json({msg: "Posts works"}));
+
+// @route     POST api/posts
+// @desc      Create new post
+// @access    Private
+router.post('/', passport.authenticate('jwt', { session: false }), (req,res) => {
+  const newPost = new Post({
+    test: req.body.text,
+    name: req.body.name,
+    avatar: req.body.name,
+    user: req.user.id
+  });
+
+  newPost.save().then(post => res.json(post));
+});
+
 
 module.exports = router;
