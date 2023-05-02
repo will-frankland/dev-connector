156f6e13b34c0bf8fdcc7555a512897308e2f5bc
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-05-01 20:59:24 -0400

    add route for liking posts

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -63,16 +63,16 @@ router.post(
   }
 );
 
-// @route     DELET api/posts/:id
+// @route     DELETE api/posts/:id
 // @desc      Delete post
 // @access    Private
 router.delete(
   "/:id",
   passport.authenticate("jwt", { session: false }),
   (req, res) => {
-    Profile.findOne({ user: req.user.id }).then(profile => {
+    Profile.findOne({ user: req.user.id }).then((profile) => {
       Post.findById(req.params.id)
-        .then(post => {
+        .then((post) => {
           // Check for post owner
           if (post.user.toString() !== req.user.id) {
             return res
@@ -82,7 +82,37 @@ router.delete(
           // Delete
           post.remove().then(() => res.json({ success: true }));
         })
-        .catch(err =>
+        .catch((err) =>
+          res.status(404).json({ postnotfound: "No post found" })
+        );
+    });
+  }
+);
+
+// @route     POST api/posts/like/:id
+// @desc      Like post
+// @access    Private
+router.post(
+  "/like/:id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id }).then((profile) => {
+      Post.findById(req.params.id)
+        .then((post) => {
+          if (
+            post.likes.filter((like) => like.user.toString() === req.user.id)
+              .length > 0
+          ) {
+            return res
+              .status(400)
+              .json({ alreadyliked: "User already liked this post" });
+          }
+          // Add user id to the likes array
+          post.likes.unshift({ user: req.user.id });
+
+          post.save().then((post) => res.json(post));
+        })
+        .catch((err) =>
           res.status(404).json({ postnotfound: "No post found" })
         );
     });
