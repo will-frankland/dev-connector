ba636fbe09f50905ece89dd9a010ad52a6512fcc
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-05-07 12:46:11 -0400

    initial landing component setup and imported to app

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,13 @@ import './App.css';
 
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
+import Landing from './components/layout/Landing';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <h1>My React App</h1>
+      <Landing />
       <Footer />
     </div>
   );
