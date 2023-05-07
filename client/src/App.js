0647e770808f8e8df03987f288a55eba05c517eb
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-05-07 13:36:07 -0400

    signup, login pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,24 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Component } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
-
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
 import Landing from "./components/layout/Landing";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
 
-class App extends Component() {
-  render() {
-    return (
-      <Router>
-        <div className="App">
-          <Navbar />
-          <Landing />
-          <Footer />
-        </div>
-      </Router>
-    );
-  }
-}
+const App = () => (
+  <BrowserRouter>
+    <div className="App">
+      <Navbar />
+      <Routes>
+        <Route path="/" exact element={<Landing />} />
+        <Route path="/register" exact element={<Register />} />
+        <Route path="/login" exact element={<Login />} />
+      </Routes>
+      <Footer />
+    </div>
+  </BrowserRouter>
+);
 
 export default App;
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Navbar extends Component {
   render() {
@@ -23,10 +24,10 @@ class Navbar extends Component {
           <div className="collapse navbar-collapse" id="mobile-nav">
             <ul className="navbar-nav mr-auto">
               <li className="nav-item">
-                <a className="nav-link" href="/profiles">
+                <Link className="nav-link" to="/profiles">
                   {" "}
                   Developers
-                </a>
+                </Link>
               </li>
             </ul>
           </div>
