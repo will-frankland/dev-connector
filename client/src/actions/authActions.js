1adc4e074d234eb5b6f951b00fb81b1062bc5807
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-05-23 10:52:22 -0400

    create setCurrentUser function and add to types

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import setAuthToken from '../utils/setAuthToken';
-import { GET_ERRORS } from "./types";
+import jwt_decode from 'jwt-decode';
+import { GET_ERRORS, SET_CURRENT_USER } from "./types";
 
 // Register User
 export const registerUser = (userData, history) => (dispatch) => {
@@ -26,6 +27,10 @@ export const loginUser = (userData) => (dispatch) => {
       localStorage.setItem('jwtToken', token);
       // Set token to auth header
       setAuthToken(token);
+      // Decode token to get user data
+      const decoded = jwt_decode(token);
+      // Set current user
+      dispatch(setCurrentUser(decoded));
     })
     .catch((err) =>
       dispatch({
@@ -34,3 +39,12 @@ export const loginUser = (userData) => (dispatch) => {
       })
     );
 };
+
+// Set logged in user
+
+export const setCurrentUser = (decoded) => {
+  return {
+    type: SET_CURRENT_USER,
+    payload: decoded
+  }
+}
