07972d8dabf4c0cd13eb99ae0fdba5f946992c80
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-05-23 09:41:26 -0400

    setup store file for redux and middleware functions

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,22 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Component } from "react";
 import { Provider } from "react-redux";
+import store from "./store";
 
+// Import Styling
 import "./App.css";
+
+// Import Components
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
 import Landing from "./components/layout/Landing";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 
+
+
 const App = () => (
+  <Provider store={ store }>
   <BrowserRouter>
     <div className="App">
       <Navbar />
@@ -21,6 +28,7 @@ const App = () => (
       <Footer />
     </div>
   </BrowserRouter>
+  </Provider>
 );
 
 export default App;
