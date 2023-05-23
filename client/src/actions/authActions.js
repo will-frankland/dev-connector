f6dee65258b6ff3b60dfff72430c684fdbb41bd9
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-05-23 10:24:23 -0400

    add axios call and dispatch to registerUser authActions file

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,23 +1,28 @@
-import { TEST_DISPATCH } from "./types"
 import axios from "axios";
+import { TEST_DISPATCH } from "./types";
 
 // Register User
-export const registerUser =(userData) => {
-  return {
-    type: TEST_DISPATCH,
-    payload: userData
-  };
+export const registerUser = (userData) => (dispatch) => {
+  axios
+    .post("/api/users/register", userData)
+    .then((res) => console.log(res.data))
+    .catch((err) =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data,
+      })
+    );
 };
 
 // Login - Get User Token
-export const loginUser = (userData) => dispatch => {
-  axios.post('/api/users/login', userData)
-    .then(res => {
-
-    })
-    .catch(err => 
+export const loginUser = (userData) => (dispatch) => {
+  axios
+    .post("/api/users/login", userData)
+    .then((res) => {})
+    .catch((err) =>
       dispatch({
         type: GET_ERRORS,
-        payload: err.response.data
-      }));
+        payload: err.response.data,
+      })
+    );
 };
